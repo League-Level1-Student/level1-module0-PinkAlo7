25d6c16674582b4f1d08b1c30899f6eaf1bb39b2
league <matthewseul>
2024-02-14 16:58:28 -0800

    2/14/24 Today, I did Flappy bird.

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -9,10 +9,14 @@ public class FlappyBird extends PApplet {
 	static final int HEIGHT = 600;
 	int positionX = 594;
 	int positionY = 50;
-	int birdVelocity = -10;
+	int birdVelocity = -9;
 	int gravity = 1;
 	int X = 800;
-    Random number = new Random();
+	int pipeGap = 60;
+	Random number = new Random();
+	int upperPipeHeight = (int) random(100, 400);
+	int lowerY = pipeGap + upperPipeHeight;
+
 	@Override
 	public void settings() {
 
@@ -30,30 +34,49 @@ public class FlappyBird extends PApplet {
 		fill(235, 52, 213);
 		stroke(0, 0, 0);
 		ellipse(positionX, positionY, 20, 20);
-		birdVelocity+=gravity;
-		fill(10,166,80);
-		stroke(0,0,0);
-	    X-=1;
-	    if(X == 0) {
-	    	X = 800;
-	    }
-	 		rect(X,0,60,200);
-        
+		birdVelocity += gravity;
+		fill(10, 166, 80);
+		stroke(0, 0, 0);
+		rect(X, 0, 60, upperPipeHeight);
+		fill(10, 166, 80);
+		stroke(0, 0, 0);
+		rect(X, lowerY, 60, 350);
+		teleportPipes();
 
-	if(mousePressed) {
-		println("Mouse’s x-position: " + mouseX + "\n" + "Mouse’s y-position: " + mouseY + "\n");
-	
+		if (mousePressed) {
+			println("Mouse’s x-position: " + mouseX + "\n" + "Mouse’s y-position: " + mouseY + "\n");
+
+		}
 	}
-	}
+
 	public void mousePressed() {
-	 birdVelocity = -10;
+		birdVelocity = -9;
 	}
+
 	public void teleportPipes() {
-		
+
+		X -= 1;
+		if (X == 0) {
+			X = 800;
+			upperPipeHeight = (int) random(100, 400);
+			lowerY = upperPipeHeight + pipeGap;
+		}
+
 	}
+
+	boolean intersectsPipes() {
+		if (positionY < upperPipeHeight && positionX > X && positionX < (X + 60)) {
+			return true;
+		} else if (positionY > lowerPipeTop && positionX > X && positionX < (X + 60)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	
 
 	static public void main(String[] args) {
-        PApplet.main(FlappyBird.class.getName());
-    }
+		PApplet.main(FlappyBird.class.getName());
+	}
 }
