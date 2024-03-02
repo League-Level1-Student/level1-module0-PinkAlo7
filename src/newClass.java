1a7c233975dcbbacead688dd4988e2ec3f81b0b6
Pink Alo <Alo@fakeemail.com>
2024-03-01 18:03:28 -0800

    3/1/24 Today, I only worked on a new class at home.

---
+++ src/newClass.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class newClass {
+	JFrame dkWindow = new JFrame(); 
+	dkWindow.setVisible(true)
+	dkWindow.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE)
+	JPanel dkPanel = new JPanel();
+	
+	JLabel dkLabel = createLabelImage("donkeykong.webp");
+	
+
+
+
+}
+
